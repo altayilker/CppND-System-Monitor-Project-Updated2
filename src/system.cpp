246939fb69c1768d7c487cf0524512d0829b1f64
ilker <ilkeraltay1981@gmail.com>
2020-07-14 14:30:47 +0300

    system is ok - processes so so 3

--- src/system.cpp
+++ src/system.cpp
@@ -42,27 +42,6 @@ vector<Process>& System::Processes() {
 // TODO: Return the system's kernel identifier (string)
 std::string System::Kernel() { return LinuxParser::Kernel(); }
 
-// TODO: Return the system's memory utilization
-float System::CpuUtilization() { 
-    
-    /*
-    
-    https://stackoverflow.com/questions/3017162/how-to-get-total-cpu-usage-in-linux-using-c/3017438#3017438
-
-    CPUSnapshot previousSnap;
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    CPUSnapshot curSnap;
-
-    const float ACTIVE_TIME = curSnap.GetActiveTimeTotal() - previousSnap.GetActiveTimeTotal();
-    const float IDLE_TIME   = curSnap.GetIdleTimeTotal() - previousSnap.GetIdleTimeTotal();
-    const float TOTAL_TIME  = ACTIVE_TIME + IDLE_TIME;
-    int usage = 100.f * ACTIVE_TIME / TOTAL_TIME;
-    std::cout << "total cpu usage: " << usage << std::endl;
-
-    */
-   return 0.18; // LinuxParser::CpuUtilization();
-    
-}
 
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
